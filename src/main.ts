b313931fbb0a73a0885f001c980bf9f26201296f
alexandre.rey <alexandre.rey74@outlook.com>
2024-06-02 21:02:03 +0200

    styling results

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import { lineY, plot, ruleY } from '@observablehq/plot';
+import { crosshair, dot, lineY, plot, pointer, ruleY } from '@observablehq/plot';
 import { dataset } from './data'
 import './style.css'
 import { computeCost, computeGradient, gradientDescent } from './linear.regression';
@@ -21,15 +21,13 @@ const result = gradientDescent(normalizedX, normalizedX, w_init, b_init, alpha, 
 
 console.log(`f"(w,b) found by gradient descent: (${result.w}, ${result.b})`);
 
-const normalizedResult = [
-  { x: 0, y: result.w * 0 + result.b },
-  { x: 0.5, y: result.w * 0.5 + result.b },
-  { x: 1, y: result.w * 1 + result.b }
-]
+const predictValue = (input: number): number => {
+  return denormalizeInput(result.w * normalizeInput(input, xTrain) + result.b, yTrain);
+}
 
-const test = result.w * normalizeInput(1250, xTrain) + result.b
-console.log('raw output: ', test);
-console.log('denormalized output: ', denormalizeInput(test, yTrain));
+/**
+ * Normalized Graph
+ */
 
 const normalizedData = normalizedX.map((x, i) => {
   return {
@@ -37,6 +35,12 @@ const normalizedData = normalizedX.map((x, i) => {
     y: normalizedY[i]
   }
 }).sort((a, b) => a.x - b.x);
+
+const normalizedResult = [
+  { x: 0, y: result.w * 0 + result.b },
+  { x: 0.5, y: result.w * 0.5 + result.b },
+  { x: 1, y: result.w * 1 + result.b }
+]
 
 const normalizedGraph = plot({
   marginLeft: 50,
@@ -47,6 +51,10 @@ const normalizedGraph = plot({
   ]
 });
 
+/**
+ * Original Graph
+ */
+
 const rawChartData = xTrain.map((x, i) => {
   return {
     x: x,
@@ -54,17 +62,28 @@ const rawChartData = xTrain.map((x, i) => {
   }
 }).sort((a, b) => a.x - b.x);
 
+const resultData = [852, 1250, 4000, 4478].map(val => {
+  return {
+    x: val,
+    y: predictValue(val) / 1_000
+  }
+})
+
+
 const rawGraph = plot({
   marks: [
     ruleY([0]),
     lineY(rawChartData, { x: 'x', y: 'y' }),
-  ]
+    lineY(resultData, { x: 'x', y: 'y', stroke: 'green' }),
+  ],
 })
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
+  <div class="card">
     <p>w: ${result.w} b: ${result.b}</p>
-    ${normalizedGraph.outerHTML}
-    ${rawGraph.outerHTML}
+    <div class="graph">
+      ${normalizedGraph.outerHTML}
+      ${rawGraph.outerHTML}
+    </div>
   </div>
 `
